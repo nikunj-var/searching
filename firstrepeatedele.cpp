856eee929fc738faba369d5d7290e8fecb38eebd
Nikunj <nikunjvar2002@gmail.com>
2022-07-12 19:21:30 -0700

    searching

--- firstrepeatedele.cpp
+++ firstrepeatedele.cpp
@@ -4,35 +4,35 @@
 //tc-O(n^2)
 //sc-O(1)
 
-// #include<iostream>
-// using namespace std;
+#include<iostream>
+using namespace std;
 
-// int firstrepeated(int arr[],int n){
+int firstrepeated(int arr[],int n){
 
-//     for(int i=0;i<n;i++){
-//         for(int j=i+1;j<n;j++){
-//             if(arr[i]==arr[j])
-//                 return arr[i];
-//         }
-//     }
-//     return -1;
-// }
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]==arr[j])
+                return arr[i];
+        }
+    }
+    return -1;
+}
 
-// int main(){
-//     int n;
+int main(){
+    int n;
     
-//     cout<<"enter size of array";
-//     cin>>n;
+    cout<<"enter size of array";
+    cin>>n;
     
-//     int arr[n];
-//     cout<<"enter array";
+    int arr[n];
+    cout<<"enter array";
     
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
 
-//    cout<<"first repeated element is"<<firstrepeated(arr,n);
-// }
+   cout<<"first repeated element is"<<firstrepeated(arr,n);
+}
 
 //WE CANNOT APPLY SORTING APPROACH IN THIS QUESTION
 
---
+++ repeatedelement.cpp
@@ -0,0 +1,110 @@
+//IN A ARRAY ALL THE ELEMENTS OCCUR ONLY ONCE EXCEPT TWO ELEMENTS FIND THOSE ELEMENTS.
+
+
+//approach1-brute force
+//tc-O(n^2)
+//sc-O(1)
+//#include<iostream>
+// using namespace std;
+
+// void twotimeoccurence(int arr[],int n){
+//     for(int i=0;i<n;i++){
+//         for(int j=i+1;j<n;j++){
+//             if(arr[i]==arr[j]){
+//                 cout<<arr[i]<<endl;
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+// int main(){
+//     int n;
+    
+//     cout<<"enter size of array";
+//     cin>>n;
+    
+//     int arr[n];
+//     cout<<"enter array";
+    
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+
+//     twotimeoccurence(arr,n);
+// }
+
+//approach2-sort array then scan
+//tc-O(nlogn)
+//sc-O(1)
+
+
+// #include<iostream>
+// #include<algorithm>
+// using namespace std;
+
+// void twotimeoccurence(int arr[],int n){
+//     for(int i=0;i<n-1;i++){
+//         if(arr[i]==arr[i+1])
+//             cout<<arr[i]<<endl;
+//     }
+// }
+
+// int main(){
+//     int n;
+    
+//     cout<<"enter size of array";
+//     cin>>n;
+    
+//     int arr[n];
+//     cout<<"enter array";
+    
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+
+//     sort(arr,arr+n);
+
+//     twotimeoccurence(arr,n);
+// }
+
+
+//approach3-create a count array of size n if the count[i]==2 print it
+//tc-O(n)
+//sc-O(n)
+
+
+//approach4-using XOR
+//tc-O(n)
+//sc-O(n)
+
+// #include<iostream>
+// #include<algorithm>
+// using namespace std;
+
+// void twotimeoccurence(int arr[],int n){
+    
+//     int =arr[0];
+//     int x,y;
+
+//     for(int i=0;i<n;i++){
+
+//     }
+
+// }
+
+// int main(){
+//     int n;
+    
+//     cout<<"enter size of array";
+//     cin>>n;
+    
+//     int arr[n];
+//     cout<<"enter array";
+    
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+
+//     twotimeoccurence(arr,n);
+// }
