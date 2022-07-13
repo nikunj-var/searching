ac0f38df91be5819da11ae0fdb98dc63d2180d25
Nikunj <nikunjvar2002@gmail.com>
2022-07-12 20:17:42 -0700

    searching

--- twosum.cpp
+++ twosum.cpp
@@ -1,65 +1,126 @@
-// //approach1-brute force
-// //tc-O(n^2)
-// //sc-O(1)
+// // //approach1-brute force
+// // //tc-O(n^2)
+// // //sc-O(1)
 
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-void twosum(int arr[],int n,int target){
-    for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            if(arr[i]+arr[j]==target){
-                cout<<arr[i]<<" "<<arr[j]<<endl;
-                return;
-            }
-        }
-    }
-    cout<<"no solution";
-}
+// void twosum(int arr[],int n,int target){
+//     for(int i=0;i<n;i++){
+//         for(int j=i;j<n;j++){
+//             if(arr[i]+arr[j]==target){
+//                 cout<<arr[i]<<" "<<arr[j]<<endl;
+//                 return;
+//             }
+//         }
+//     }
+//     cout<<"no solution";
+// }
 
-int main(){
-    int n;
-    int target;
-    cout<<"enter size of array";
-    cin>>n;
+// int main(){
+//     int n;
+//     int target;
+//     cout<<"enter size of array";
+//     cin>>n;
     
-    cout<<"enter target value";
-    cin>>target;
+//     cout<<"enter target value";
+//     cin>>target;
 
-    int arr[n];
-    cout<<"enter array";
+//     int arr[n];
+//     cout<<"enter array";
     
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
 
-    twosum(arr,n,target);
-}
+//     twosum(arr,n,target);
+// }
 
-// //approach2-sort the array and apply two pointer approach
-// //tc-O(nlogn)
-// //sc-O(1)
+// // //approach2-sort the array and apply two pointer approach
+// // //tc-O(nlogn)
+// // //sc-O(1)
+
+// #include<iostream>
+// #include<algorithm>
+// using namespace std;
+
+// void twosum(int arr[],int n,int target){
+//     int i=0;
+//     int j=n-1;
+//     while(i<j){
+//         if(arr[i]+arr[j]==target){
+//             cout<<arr[i]<<" "<<arr[j]<<endl;
+//             return;
+//         }
+//         else if((arr[i]+arr[j])>target){
+//             j--;   
+//         }
+//         else{
+//             i++;
+//         }
+//     }
+//     cout<<"no solution exists";
+// }
+
+
+// int main(){
+//     int n;
+//     int target;
+//     cout<<"enter size of array";
+//     cin>>n;
+    
+//     cout<<"enter target value";
+//     cin>>target;
+
+//     int arr[n];
+//     cout<<"enter array";
+    
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+
+//     sort(arr,arr+n);
+
+//     twosum(arr,n,target);
+// }
+
+//approach3-use hashtable
+//tc-O(n)
+//sc-O(n)
+
+
+
+//QUESTION - GIVEN AN ARRAY FIND THREE INDICES SUCH THAT A[i]^2+A[j]^2=A[k]^2
+
+//approach-sort the array and for each array compute A[i]^2 and store in array.
+//tc-O(n^2)
+//sc-O(1)
 
 #include<iostream>
 #include<algorithm>
 using namespace std;
 
-void twosum(int arr[],int n,int target){
+void threesum(int arr[],int n,int target){
+    for(int i=0;i<n;i++){
+        arr[i]==arr[i]*arr[i];
+    }
+
     int i=0;
     int j=n-1;
-    while(i<j){
-        if(arr[i]+arr[j]==target){
-            cout<<arr[i]<<" "<<arr[j]<<endl;
+    
+    while(i < j){
+        if((arr[i] + arr[j])==target){
+            cout<<arr[i]<<" "<<arr[j]<<" "<<target<<endl;
             return;
         }
-        else if((arr[i]+arr[j])>target){
-            j--;   
+        else if((arr[i] + arr[j])>target){
+            j--;
         }
         else{
             i++;
         }
     }
-    cout<<"no solution exists";
+    cout<<"does not exists";
 }
 
 
@@ -81,11 +142,5 @@ int main(){
 
     sort(arr,arr+n);
 
-    twosum(arr,n,target);
+    threesum(arr,n,target);
 }
-
-//approach3-use hashtable
-//tc-O(n)
-//sc-O(n)
-
-
